9cb5181cf1a735c20c4fc053367fbcae198efc81
chandra sekhar avula <cavula@innominds.com>
2017-01-02 17:29:05 +0530

    url changed

--- jenkins/seedjobs/edmpSeedJob.groovy
+++ jenkins/seedjobs/edmpSeedJob.groovy
@@ -16,7 +16,7 @@ println "#######################################################################
 println "Create Default Views and Admin Jobs"
 println ""
 
-def edmpGitUrl="https://github.com/codecentric/event-driven-microservices-platform"
+def edmpGitUrl="https://github.com/omkar100100/event-driven-microservices-platform-master"
 globalProdNetwork="eventdrivenmicroservicesplatform_prodnetwork"
 createDockerJob("docker-admin-version", "", "sudo /usr/bin/docker version", "")
 createDockerJob("docker-admin-list-running-container", "", "sudo /usr/bin/docker ps", "")
